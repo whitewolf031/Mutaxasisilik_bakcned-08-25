3e6c2a8c33664f4235394683b4e7c9625fbdc3ee
Sardorbek031 <w72075433@gmail.com>
2025-12-05 17:09:01 +0500

    finish teh git lesson 50%

---
+++ git_bilan_ishlash/gitbilanishlash.py
@@ -0,0 +1,16 @@
+# testing_repo dan qilish
+# git clone https://github.com/whitewolf031/Testing_repo.git
+
+# git branch - branch ni ko'rish
+# git checkout -b world - yangi branch ochish
+# git push origin world - git ga joylash
+
+# Yangi branch yaratib o'shani joylab merge request tashash
+
+# git ga yangi loyiha joylash
+# git init - git ga ulanish
+# git add . - git ga saqlash
+# git commit -m "comment" - comment yozib saqlash
+# git branch -M main
+# git remote add origin git@github.com:whitewolf031/Mutaxasisilik_bakcned-08-25.git
+# git push origin main
